ed38b9caff04a4638f7e5ff80f91616c42a75636
kanaOwl <100942036+kanaOwl@users.noreply.github.com>
2022-06-15 00:31:51 +0300

    Add files via upload

---
+++ PP_2.2.1_Spring_Hibernate/2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -0,0 +1,52 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+      Car audi = new Car("Audi A3", 1324);
+      Car bmw = new Car("BMW X5", 549);
+      Car dodge = new Car("Dodge RAM 3500", 9670);
+      Car ford = new Car("Ford Focus", 47);
+
+      user1.setCar(audi);
+      user2.setCar(bmw);
+      user3.setCar(dodge);
+      user4.setCar(ford);
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = " + user.getId());
+         System.out.println("First Name = " + user.getFirstName());
+         System.out.println("Last Name = " + user.getLastName());
+         System.out.println("Email = " + user.getEmail());
+         System.out.println();
+      }
+
+      System.out.println(userService.getUser(1324, "Audi A3").toString());
+      System.out.println(userService.getUser(47,"Ford Focus").toString());
+
+      context.close();
+   }
+}
